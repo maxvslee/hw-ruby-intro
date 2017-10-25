51149a7892d18209aa0568fd6b3cd43d313daa71
Max Lee <maxvslee@gmail.com>
2017-10-25 08:47:13 +0000

    Updated program

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,40 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  if arr.size == 0
+    sum = 0
+  else
+    arr.each { |i| sum= sum + i }
+  end
+  sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.size == 0
+    m2s = 0
+  elsif arr.size == 1
+    m2s = arr[0]
+  else
+    arr = arr.sort
+    m2s = arr[-1] + arr[-2]  
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  b=arr.size-1
+  flag = false  
+  0.upto(b) do |i|
+    i.upto(b) do |j|
+      if i != j
+        sum = arr[i] + arr[j]
+        if sum == n
+          flag = true
+        end   
+      end
+    end
+  end
+  flag
 end
 
 # Part 2
@@ -31,5 +56,20 @@ end
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = price
+  end
+  def isbn ; @isbn ; end
+  def isbn=(new_isbn)
+    if new_isbn  
+      @isbn = new_isbn
+    end
+  end
+  def price ; @price ; end
+  def price=(new_price) ; @price = new_price ; end
+  
+  def price_as_string price
+    @price = sprintf("$%.2f", price)
+  end
 end
